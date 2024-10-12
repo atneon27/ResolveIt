9bd688dc9fe40bb1b3a19b6ba9106725efc7c357
atneon27 <aryan.tiwari.at27@gmail.com>
2024-10-12 23:36:34 +0530

    Improved Student Route

--- backend/src/student.ts
+++ backend/src/student.ts
@@ -2,9 +2,30 @@ import express, { Request, Response } from "express";
 import { Complain } from "./db";
 const router = express.Router();
 
-router.get('/about', (req, res) => {
+router.post('/raiseComplain', async (req, res) => {
+    const user = await Complain.create({
+        raisedBy: req.body.raisedBy,
+        body: {
+            hostel: req.body.hostel,
+            wing: req.body.wing,
+            room: req.body.room,
+            complainSubject: req.body.complainSubject,
+            complainBody: req.body.complainBody,
+        }
+    });
+
+    console.log(user);
     res.status(200).json({
-        msg: "This is the Student Route Handler"
+        msg: "complain raised",
+        reffId: user._id,
     })
-})
+});
+
+router.get('/complains', async(req, res) => {
+    const result = await Complain.find({});
+    res.status(200).json({
+        result: result,
+    })
+});
+
 export default router;
