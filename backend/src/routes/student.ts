f398bfc8f15b2bbbe844a063d5019530b9925ac6
atneon27 <aryan.tiwari.at27@gmail.com>
2024-10-13 23:35:37 +0530

    imporved student route

---
+++ backend/src/routes/student.ts
@@ -0,0 +1,32 @@
+import express, { Request, Response } from "express";
+import { Complain } from "../db/schema";
+const router = express.Router();
+
+router.post('/raiseComplain', async (req, res) => {
+    const user = await Complain.create({
+        userType: "Student",
+        raisedBy: req.body.raisedBy,
+        body: {
+            hostel: req.body.hostel,
+            wing: req.body.wing,
+            room: req.body.room,
+            complainSubject: req.body.complainSubject,
+            complainBody: req.body.complainBody,
+        }
+    });
+
+    console.log(user);
+    res.status(200).json({
+        msg: "complain raised",
+        reffId: user._id,
+    })
+});
+
+router.get('/complains', async(req, res) => {
+    const result = await Complain.find({});
+    res.status(200).json({
+        result: result,
+    })
+});
+
+export default router;
