859013c57c74dd47e9ea8ce82fec506a24a95801
atneon27 <aryan.tiwari.at27@gmail.com>
2024-10-13 23:35:58 +0530

    added auth route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
-import StudentRouter from "./student";
+import StudentRouter from "./routes/student";
+import AuthRouter from "./routes/auth";
 
 const app = express();
 app.use(express.json());
 
+app.use('/auth', AuthRouter);
 app.use('/student', StudentRouter);
 
 app.listen(3000);
