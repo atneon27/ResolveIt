02b702117aef6c402ccb46a11f20d6b2be649854
atneon27 <aryan.tiwari.at27@gmail.com>
2024-10-15 23:36:01 +0530

    improved login validation

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,7 +1,7 @@
 import express, { Response, Request } from "express";
 import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "../secret";
-import { Students } from "../db/schema";
+import { Admins, Students, Workers } from "../db/schema";
 
 const router = express.Router();
 
@@ -33,44 +33,121 @@ type WorkerDetails = {
 }
 
 router.post('/student/signup', async (req, res) => {
-    const User: StudentDetails = req.body;
-
-    const student = await Students.create({
-        regno: User.reg_no,
-        password: User.password,
-        details: {
-            currentHostel: User.currentHostel,
-            currentWing: User.currentWing,
-            currentRoom: User.currentRoom
+    const StudentUser: StudentDetails = req.body;
+    try {
+        const existingStudent = await Students.findOne({
+            reg_no: StudentUser.reg_no
+        });
+        if(existingStudent) {
+            res.status(400).json({
+                err: "Email Already Exists!"
+            });
         }
-    });
-
-    res.status(200).json({
-        msg: "Student Created"
-    })
+        
+        const student = await Students.create({
+            regno: StudentUser.reg_no,
+            password: StudentUser.password,
+            details: {
+                currentHostel: StudentUser.currentHostel,
+                currentWing: StudentUser.currentWing,
+                currentRoom: StudentUser.currentRoom
+            }
+        });
+    
+        res.status(201).json({
+            msg: "Student Registered Successfully!"
+        });
+    } catch (err) {
+        res.status(500).json({
+            msg: "Internal Server Error"
+        });
+    }
 });
 
 router.post('/student/signin', async(req, res) => {
     const {reg_no, password}: StudentDetails = req.body;
+    try {
+        const student = await Students.find({
+            reg_no,
+            password
+        });
+
+        if(student) {
+            const token = jwt.sign({
+                reg_no
+            }, JWT_SECRET);
+            res.status(200).json({
+                token
+            });
+        } else {
+            res.status(500).json({
+                msg: "Internal Error!"
+            });
+        }
+    } catch(err) {
+        res.status(400).json({
+            msg: "Internal Server Error!"
+        });
+    }
+});
+
+router.post('/admin/signup', async (req, res) => {
+    const AdminUser: AdminDetails = req.body;
+
+    const admin = await Admins.create({
+        admin_id: AdminUser.admin_id,
+        password: AdminUser.password,
+        firstname: AdminUser.firstname,
+        lastname: AdminUser.lastname,
+        dpt_name: AdminUser.dpt_name,
+        assignedHostel: AdminUser.assignedHostel,
+    });
+
+    res.status(200).json({
+        msg: "Admin Created!"
+    });
+});
+
+router.post('/admin/signin', async(req, res) => {
+    const { admin_id, password }: AdminDetails = req.body;
 
     const student = await Students.find({
-        reg_no,
+        admin_id,
         password
     });
 
     if(student) {
         const token = jwt.sign({
-            reg_no,
+            admin_id
         }, JWT_SECRET);
         res.status(200).json({
-            user_token: token,
-        })
+            token: token
+        });
     } else {
-        res.status(404).json({
-            msg: "an error",
+        res.status(200).json({
+            msg: "Error while Signin"
         })
     }
 });
 
+router.post('/worker/signup', async (req, res) => {
+    const WorkerUser: WorkerDetails = req.body;
+
+    const worker = await Workers.create({
+        worker_id: WorkerUser.worker_id,
+        password: WorkerUser.password,
+        firstname: WorkerUser.firstname,
+        lastname: WorkerUser.lastname,
+        assignedHostel: WorkerUser.assignedHostel
+    });
+
+    res.status(200).json({
+        msg: "Worker Created!"
+    })
+});
+
+router.post('/worker/singin', (req, res) => {
+    
+});
 
 export default router;
