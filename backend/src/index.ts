9a4dd5083febdd598e01f0d66aedf79920d5f687
atneon27 <aryan.tiwari.at27@gmail.com>
2024-10-17 22:23:14 +0530

    added admin route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import StudentRouter from "./routes/student";
+import AdminRouter from "./routes/admin";
 import AuthRouter from "./routes/auth";
 
 const app = express();
@@ -7,5 +8,6 @@ app.use(express.json());
 
 app.use('/auth', AuthRouter);
 app.use('/student', StudentRouter);
+app.use('/admin', AdminRouter);
 
 app.listen(3000);
