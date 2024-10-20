721921e2367ffa700075158b0db471e1be7cf33a
atneon27 <aryan.tiwari.at27@gmail.com>
2024-10-20 22:44:53 +0530

    updated middlerware

--- backend/src/routes/middleware.ts
+++ backend/src/routes/middleware.ts
@@ -1,6 +1,10 @@
 import { NextFunction, Request, Response } from "express";
-import jwt from "jsonwebtoken";
+import jwt, { JwtPayload } from "jsonwebtoken";
 import { JWT_SECRET } from "../secret";
+
+interface CustomRequest extends Request {
+    user_data: string | JwtPayload,
+}
 
 export default function AuthMiddleware(req: Request, res: Response, next: NextFunction): void {
     const token = req.headers.authorization ?? '';
@@ -14,6 +18,7 @@ export default function AuthMiddleware(req: Request, res: Response, next: NextFu
 
     try {
         const decode = jwt.verify(token, JWT_SECRET);
+        (req as CustomRequest).user_data = decode;
         next(); 
     } catch(err) {
         res.status(400).json({
